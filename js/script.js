aadf5eb2cd03b4241b9ca9be4cad291087565dc8
hidri <agthidri2400@gmail.com>
2020-04-09 23:38:33 +0300

    this commit wil add javascript code

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $("#what-image").click(function(){
+        $("#service-design").show();
+        $("#what-image").hide();
+
+
+       
+    })
+    $("#service-design").click(function(){
+        $("#what-image").show();
+       $("#service-design").hide();
+       
+    })
+    $("#what-image1").click(function(){
+        $("#service-dev").show();
+        $("#what-image1").hide();
+    })
+    $("#service-dev").click(function(){
+        $("#what-image1").show();
+       $("#service-dev").hide();
+       
+    })
+    $("#what-image2").click(function(){
+        $("#service-pro").show();
+        $("#what-image2").hide();
+    })
+    $("#service-pro").click(function(){
+        $("#what-image2").show();
+       $("#service-pro").hide();
+       
+    })
+    $("#work-image").click(function(){
+        $("#delani-text").toggle();
+    })
+    $("#work-image1").click(function(){
+        $("#delani-text1").show();
+    })
+    $("#work-image2").click(function(){
+        $("#delani-text2").show();
+    })
+
+    $("#work-image3").click(function(){
+        $("#delani-text3").show();
+    })
+    $("#work-image3").click(function(){
+        $("#delani-text4").show();
+    })
+    $("#work-image44").click(function(){
+        $("#delani-text44").show();
+    })
+    $("#work-image5").click(function(){
+        $("#delani-text5").show();
+    })
+    $("#work-image6").click(function(){
+        $("#delani-text6").show();
+    })
+    $("#work-image7").click(function(){
+        $("#delani-text7").show();
+    })
+    
+
+});
+$ (function(){
+    new WOW().init();   
+});
+$(window).on('load',function(){
+    $("#").addClass("animated fadeInDown");
+    $("#").addClass("animated fadeInRight");
+    $("#").addClass("animated zoomIn");
+    $("#").addClass("animated fadeDown infinite");
+
+});
